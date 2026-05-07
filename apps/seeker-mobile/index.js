15ca4b6b4eadfda4b1a3c0e62820bb287ccf074d
nullxnothing <dspivack2@student.cccs.edu>
2026-05-06 19:04:22 -0600

    feat: add Seeker mobile entrypoint

---
+++ apps/seeker-mobile/index.js
@@ -0,0 +1,8 @@
+import 'react-native-get-random-values'
+import { Buffer } from 'buffer'
+import { registerRootComponent } from 'expo'
+import App from './App'
+
+global.Buffer = global.Buffer || Buffer
+
+registerRootComponent(App)
